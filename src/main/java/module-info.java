84dbe5c508b20af8bae9acb2c3fd3a9945b63b21
Pabodya Kandambi <youremail@example.com>
2025-11-28 09:52:45 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.vihangielectronics_ijse_76 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.vihangielectronics_ijse_76.controller to javafx.fxml;
+    exports lk.ijse.vihangielectronics_ijse_76;
+}
